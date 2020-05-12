502a694bedf0b0787f033177fcf54f422cd0d3b9
Naftali Ajnwojner <naftali.ajnwojner@gmail.com>
2020-05-12 16:16:36 +0300

    restaurants app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+wuhan = { name: "Wuhan", address: "Frankfurt", phone_number: '03-773-1234',category: "chinese" }
+festival = { name: "Festival", address: "Knokke", phone_number: '03-000-3770',category: "belgian" }
+pizza = { name: 'Pizza Juliet', address: 'Ha-Galil St 78, Ganei Tikva', phone_number: '03-773-3770', category: 'italian' }
+sushi = { name: 'Oshi Oshi Sushi', address: '17, HaKalanit St 1, Kiryat Ono', phone_number: '03-575-3012', category: 'japanese' }
+tony = { name: 'Tony Vespa', address: 'HaShikma St 1, Savyon', phone_number: '03-542-4730', category: 'italian' }
+
+[ wuhan, festival, pizza, sushi, tony ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
